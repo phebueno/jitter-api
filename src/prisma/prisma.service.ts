8a657c17c5ff02251c58d3984768a208ee8889f3
Fernando Bueno <fcbgomes@gmail.com>
2025-12-01 19:55:57 -0300

    fix: configure Prisma adapter-pg and unify database connection logic

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,30 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const {
+      POSTGRES_USER = 'jitter',
+      POSTGRES_PASSWORD = 'jitter123',
+      POSTGRES_HOST = 'localhost',
+      POSTGRES_PORT = '5432',
+      POSTGRES_DB = 'jitter_db',
+    } = process.env;
+
+    const connectionString = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?schema=public`;
+    
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
