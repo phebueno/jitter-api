89eff86f1ef397c3bd9bc15671c67be8f9930c2e
Fernando Bueno <fcbgomes@gmail.com>
2025-12-01 21:10:53 -0300

    refactor: rename GET /products to /product/list

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
-@Controller('products')
+@Controller('product')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Get()
+  @Get('list')
   findAll() {
     return this.productsService.findAll();
   }
